686edb9dc7ec65bf74060e9bc57ff6072c9e31c0
Thomas Tilahun <thomm.sef@gmail.com>
2021-02-04 10:43:51 -0800

    add job_titile in json data

--- frontend/src/peoples.js
+++ frontend/src/peoples.js
@@ -1,26 +1,26 @@
-const peoples = [
+// const peoples = [
     
-{_id:1,name:"Claudianus",email:"cbeadnall0@woothemes.com",age:31},
-{_id:2, name:"Carree",email:"crunciman1@state.gov",age:26},
-{_id:3, name:"Win",email:"wbardwell2@admin.ch",age:55},
-{_id:4,name:"Buffy",email:"bmosley3@craigslist.org",age:29},
-{_id:5,name:"Ephrem",email:"eclark4@disqus.com",age:41},
-{_id:6,name:"Avictor",email:"avagg5@dmoz.org",age:43},
-{_id:7,name:"Raquel",email:"rtopping6@ovh.net",age:27},
-{_id:8,name:"Porty",email:"pmadle7@yandex.ru",age:29},
-{_id:9,name:"Regine",email:"rhansen8@wiley.com",age:22},
-{_id:10,name:"Kristan",email:"kerickssen9@bigcartel.com", age:32},
-{_id:11,name:"Mathew",email:"mtomashova@zimbio.com",age :30},
-{_id:12,name:"Burt",email:"bwalcherb@hibu.com",age:19},
-{_id:13,name:"Eunice",email:"estoyellc@mapy.cz",age:29},
-{_id:14,name:"Clareta",email:"cketchend@purevolume.com",age:23},
-{_id:15,name:"Jamal",email:"jlauriee@merriam-webster.com",age:33},
-{_id:16,name:"Irwinn",email:"ifruchonf@reddit.com",age:43},
-{_id:17,name:"Nathan",email:"nfoxleyg@nih.gov",age:34},
-{_id:18,name:"Yorker",email:"yschimonekh@odnoklassniki.ru",age:50},
-{_id:19,name:"Renault",email:"rrozeai@ehow.com",age:36},
-{_id:20,name:"Milissent",email:"mandersj@ask.com",age:24}
+// {_id:1,name:"Claudianus",email:"cbeadnall0@woothemes.com",age:31},
+// {_id:2, name:"Carree",email:"crunciman1@state.gov",age:26},
+// {_id:3, name:"Win",email:"wbardwell2@admin.ch",age:55},
+// {_id:4,name:"Buffy",email:"bmosley3@craigslist.org",age:29},
+// {_id:5,name:"Ephrem",email:"eclark4@disqus.com",age:41},
+// {_id:6,name:"Avictor",email:"avagg5@dmoz.org",age:43},
+// {_id:7,name:"Raquel",email:"rtopping6@ovh.net",age:27},
+// {_id:8,name:"Porty",email:"pmadle7@yandex.ru",age:29},
+// {_id:9,name:"Regine",email:"rhansen8@wiley.com",age:22},
+// {_id:10,name:"Kristan",email:"kerickssen9@bigcartel.com", age:32},
+// {_id:11,name:"Mathew",email:"mtomashova@zimbio.com",age :30},
+// {_id:12,name:"Burt",email:"bwalcherb@hibu.com",age:19},
+// {_id:13,name:"Eunice",email:"estoyellc@mapy.cz",age:29},
+// {_id:14,name:"Clareta",email:"cketchend@purevolume.com",age:23},
+// {_id:15,name:"Jamal",email:"jlauriee@merriam-webster.com",age:33},
+// {_id:16,name:"Irwinn",email:"ifruchonf@reddit.com",age:43},
+// {_id:17,name:"Nathan",email:"nfoxleyg@nih.gov",age:34},
+// {_id:18,name:"Yorker",email:"yschimonekh@odnoklassniki.ru",age:50},
+// {_id:19,name:"Renault",email:"rrozeai@ehow.com",age:36},
+// {_id:20,name:"Milissent",email:"mandersj@ask.com",age:24}
 
-]
+// ]
 
-export default peoples
+// export default peoples
